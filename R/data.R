63f3ccbb7b08a0eb7881d5304b05e5976c78da94
Bibiana Terra Dasoler de Oliveira <bibianaterrad@Bibianas-MacBook-Air.local>
2021-05-28 16:43:15 -0300

    First commit

---
+++ R/data.R
@@ -0,0 +1,9 @@
+# Leitura e processamento de dados
+
+
+# persistence 
+# mamiferos nativos de medio e grande porte - periodo: jan 2015 a jan 2017
+persistence_data <- read.csv(here::here("data", "processed", "persistence_data.csv"), sep = ";")
+
+# searcher efficiency
+efficiency <- read.csv
