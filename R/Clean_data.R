db820fda2a5d5519c83d9dcea7cb58ceff53f7ad
Bibiana Terra Dasoler de Oliveira <bibianaterrad@Bibianas-MacBook-Air.local>
2021-05-28 16:13:24 -0300

    Script processamento dados

---
+++ R/Clean_data.R
@@ -0,0 +1,8 @@
+# Leitura e processamento de dados
+
+
+# persistence 
+persistence_data<-read.csv(here::here("data", "processed", "persistence_data.csv"), sep = ";")
+
+# searcher efficiency
+efficiency<-
