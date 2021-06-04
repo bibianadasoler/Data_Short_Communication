758afea0e6bf58534cb19274efbac68d0532e864
Bibiana Terra Dasoler de Oliveira <bibianaterrad@Bibianas-MacBook-Air.local>
2021-06-04 12:04:28 -0300

    Completei o processamento e leitura dos dados

--- R/data.R
+++ R/data.R
@@ -7,4 +7,18 @@ persistence_data <- read.csv(here::here("data", "processed", "persistence_data.c
 
 # searcher efficiency
 # detected e not detected se referem as carcacas vistas de auto de linha em comparacao as encontradas a pe
-efficiency <- read.csv(here::here("data", "processed", "efficiency_data.csv"), sep = ";", h = T)
+efficiency_data <- read.csv(here::here("data", "processed", "efficiency_data.csv"), sep = ";")
+
+# observed fatalities
+# registros feitos por Auto de linha (tipo de veiculo de inspecao ferroviario)
+fatality_data_filter <- openxlsx::read.xlsx(here::here("data", "raw","fatalidades_RUMO.xlsx"), detectDates = T) %>%
+  filter(malha == "Norte",
+         ano == "2015" | ano == "2016",
+         metodo == "Auto",
+         classe == "Mammalia",
+         tamanho_corporal == "Médio porte: > 500g e ??? 25kg" | tamanho_corporal == "Grande porte: > 25kg",
+         cat != "1",  # exclui animais domesticos e exoticos
+         duplicata == "nao") %>% # seleciona animais que nao foram considerados duplicatas
+      write.xlsx(., file = here::here("data", "processed","observed_data.xlsx"), asTable = T)
+
+observed_data <- openxlsx::read.xlsx(here::here("data", "processed", "observed_data.xlsx"), detectDates = T)
